7716dc43a841e5a356f7f943e7cdb8199a0fbb5d
mmaehren <32199075+mmaehren@users.noreply.github.com>
2024-03-15 16:58:02 +0100

    Allow resumption of dataset download

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Download our data sets from Zenodo (https://zenodo.org/records/10817685)
-wget https://zenodo.org/records/10817685/files/a_measurements_ground_truth.zip
-wget https://zenodo.org/records/10817685/files/b_measurements_quantitative_openssl.zip
-wget https://zenodo.org/records/10817685/files/c_measurements_quantitative_other_libraries.zip
-wget https://zenodo.org/records/10817685/files/d_measurements_qualitative.zip
-wget https://zenodo.org/records/10817685/files/e_analysis_results.zip
-wget https://zenodo.org/records/10817685/files/f_source_files_artificial_side_channels.zip
+wget -nc https://zenodo.org/records/10817685/files/a_measurements_ground_truth.zip
+wget -nc https://zenodo.org/records/10817685/files/b_measurements_quantitative_openssl.zip
+wget -nc https://zenodo.org/records/10817685/files/c_measurements_quantitative_other_libraries.zip
+wget -nc https://zenodo.org/records/10817685/files/d_measurements_qualitative.zip
+wget -nc https://zenodo.org/records/10817685/files/e_analysis_results.zip
+wget -nc https://zenodo.org/records/10817685/files/f_source_files_artificial_side_channels.zip
